f7ef1f4cd503c59445b2df27640434da6fc494ee
soyi <hushuoyi@gmail.com>
2023-03-02 19:54:57 +0800

    add

--- ChatGPT/AppDelegate/SceneDelegate.swift
+++ ChatGPT/AppDelegate/SceneDelegate.swift
@@ -16,6 +16,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let requester = Requester()
+        requester.downloadJSON { result in
+            switch result {
+            case .success(let json):
+                print(json)
+                let apikeys = json["apikeys"] as! Array<Any>
+                if apikeys.count != 0 {
+                    UserDefaults.standard.setValue(apikeys, forKey: "apikeys")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
--- ChatGPT/Utils/Requester.swift
+++ ChatGPT/Utils/Requester.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 import OpenAISwift
+import Alamofire
+import SwiftyJSON
+import Foundation
+
 
 enum APIResult {
    case success(String)
@@ -15,12 +19,27 @@ enum APIResult {
 
 final class Requester {
     //api-key
-    private static let authToken = "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"
+//    private static let authToken = "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"
+    
+    private static let authToken: String = {
+        var key = "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"
+        let apikeys = UserDefaults.standard.object(forKey: "apikeys") as! Array<Any>
+        if apikeys.count != 0 {
+            let item = apikeys.first as! Dictionary<String, Any>
+            let serverKey = item["key"] as! String
+            return serverKey
+        }
+        return key
+    }()
+    
+
     private let openAPI = OpenAISwift(authToken: authToken)
     
     func sendRequest(query: String, completion: @escaping (APIResult) -> ()) {
         
-        openAPI.sendCompletion(with: query, maxTokens: 256) { result in
+        
+        
+        openAPI.sendCompletion(with: query, maxTokens: 512) { result in
             
             switch result {
             case .success(let openApi):
@@ -40,4 +59,55 @@ final class Requester {
             }
         }
     }
+    
+    var backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+    
+
+//    let urlString = "http://rqw1qbvd3.hd-bkt.clouddn.com/authToken/keys.json?e=1677752064&token=IlnK7sx5b1XmZuHUPiHrdnpYm7tQrcMZl4evivFh:1LC80yB9hhRvmTPQFnF0ZdlhVTo="
+
+    // 下载 JSON 并将其转换为字典
+    func downloadJSON(completion: @escaping (Result<[String: Any], Error>) -> Void) {
+        guard let url = URL(string: "http://rqw1qbvd3.hd-bkt.clouddn.com/authToken/keys.json?e=1677752064&token=IlnK7sx5b1XmZuHUPiHrdnpYm7tQrcMZl4evivFh:1LC80yB9hhRvmTPQFnF0ZdlhVTo=") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                      completion(.failure(NSError(domain: "Invalid response", code: 0, userInfo: nil)))
+                      return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    completion(.success(json))
+                } else {
+                    completion(.failure(NSError(domain: "Invalid JSON", code: 0, userInfo: nil)))
+                }
+            } catch let error {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+        
+    
+    
+    func endBGTask(){
+        // End the task assertion.
+        UIApplication.shared.endBackgroundTask(self.backgroundTaskID)
+        self.backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+    }
 }
+
+
